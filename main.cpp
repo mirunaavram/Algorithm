8128c8daa2987a979fbc0d5bb09e2b95c4cd3392
mirunaavram <74984886+mirunaavram@users.noreply.github.com>
2021-11-10 01:12:18 +0200

    Add files via upload
    Havel Hakimi

--- main.cpp
+++ main.cpp
@@ -1,101 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
-ifstream f("ctc.in");
-ofstream g("ctc.out");
-vector<int> component[100001];
-vector<int> v[100001],rev[100001];
-int n,m;
 
-void add(int node1,int node2){
-    v[node1].push_back(node2);
-}
-void first_DFS(int node,int vis[100001],stack<int>& st)
-{
-    vis[node]=1;
-    for(auto i:v[node])
-        if(vis[i]==0)
-            first_DFS(i,vis,st);
-        //cout<<"aici";}
 
-    st.push(node);
-}
-void reverse(){
+int n,x;
 
-    for(int i=0;i<n;i++){
-        for(auto j:v[i])
-            rev[j].push_back(i);
-
+int verify(vector<int> grades){
+    int sum=0;
+    for(int i=0;i<grades.size();i++){
+        if(grades[i]>grades.size()-1)
+            return 0;
+        sum+=grades[i];
     }
+    if(sum %2 == 1 )
+        return 0;
+    return 1;
+
 }
-void second_DFS(int node,int vis[100001],int nr){
-    //cout<<node+1<<" ";
-    component[nr].push_back(node+1);
-    vis[node]=1;
-    for(auto i:rev[node])
-        if(vis[i]==0)
-        {
-            second_DFS(i,vis,nr);
+string answer(vector<int> grades){
+
+    if(verify(grades)){
+        while(grades.size() != 0 ){
+            sort(grades.begin(),grades.end(), greater<int>());
+            int a=grades[0];
+            grades.erase(grades.begin());
+            for(int i=0;i<a;i++){
+                if(grades[i]-1<0)
+                    return "NU";
+                else
+                    grades[i]-=1;
+            }
         }
-        //cout<<"aici";
-
-}
-void nr_SCCs(int &nr){
-    nr=0;
-    int vis[100001];
-    stack<int> st;
-    for(int i=0;i<n;i++){
-        vis[i]=0;
+        return "DA";
     }
-    for(int i=0;i<n;i++){
-        if(vis[i]==0)
-            first_DFS(i,vis,st);
-    }
-    /*while(!st.empty()){
-        //cout<<st.top()<<" ";
-        st.pop();
-    }*/
-
-    reverse();
-
-    for(int i=0;i<n;i++){
-        vis[i]=0;
-    }
-    while(!st.empty()){
-
-        int a=st.top();
-        //cout<<a<<" ";
-        st.pop();
-        if(vis[a]==0){
-            nr++;
-            second_DFS(a,vis,nr);
-            //cout<<'\n';
-
-        }
-    }
-    //cout<<nr;
+    return "NU";
 }
 
 int main(){
-    int x,y;
-//cin>>n>>m;
-f>>n>>m;
-for(int i=0;i<m;i++){
-    //cin>>x>>y;
-    f>>x>>y;
-    add(x-1,y-1);
-}
-int nr=0;
-nr_SCCs(nr);
 
-//cout<<nr<<endl;
-g<<nr<<'\n';
-for(int i=1;i<=nr;i++)
+    vector<int> grades;
+    //int n = sizeof(grades) / sizeof(grades[0]);
+    cin>>n;
+for(int i=0;i<n;i++)
 {
-    for(auto j:component[i])
-        //cout<<j<<" ";
-        g<<j<<" ";
-    //cout<<endl;
-    g<<'\n';
+    cin>>x;
+    grades.push_back(x);
 }
+    //n = sizeof(grades) / sizeof(grades[0]);
+cout<<answer(grades);
 
 }
